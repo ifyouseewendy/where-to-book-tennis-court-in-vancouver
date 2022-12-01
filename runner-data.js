40de78db4a4a15bcff831a3f9c7795077d520b9f
Di Wen <di.wen@shopify.com>
2022-11-30 22:07:39 -0800

    Update runner-data

--- runner-data.js
+++ runner-data.js
@@ -1,1 +1,75 @@
-var runnerData = {"vacancies":{"btc":{"Thu Dec 01, 2022":[{"venue":"btc","date":"Thu Dec 01, 2022","start_time":"09:00 AM","end_time":"10:00 AM","duration":"1.0h","court_info":"Indoor Court 3"},{"venue":"btc","date":"Thu Dec 01, 2022","start_time":"09:00 AM","end_time":"10:00 AM","duration":"1.0h","court_info":"Indoor Court 5"},{"venue":"btc","date":"Thu Dec 01, 2022","start_time":"01:00 PM","end_time":"02:00 PM","duration":"1.0h","court_info":"Indoor Court 2"},{"venue":"btc","date":"Thu Dec 01, 2022","start_time":"03:00 PM","end_time":"12:00 AM","duration":"-15.0h","court_info":"Indoor Court 2"},{"venue":"btc","date":"Thu Dec 01, 2022","start_time":"03:00 PM","end_time":"04:00 PM","duration":"1.0h","court_info":"Indoor Court 3"}],"Fri Dec 02, 2022":[{"venue":"btc","date":"Fri Dec 02, 2022","start_time":"08:00 AM","end_time":"08:30 AM","duration":"0.5h","court_info":"Indoor Court 1"},{"venue":"btc","date":"Fri Dec 02, 2022","start_time":"09:00 AM","end_time":"09:30 AM","duration":"0.5h","court_info":"Indoor Court 1"},{"venue":"btc","date":"Fri Dec 02, 2022","start_time":"05:00 PM","end_time":"05:30 PM","duration":"0.5h","court_info":"Indoor Court 1"}]}},"venues":{"btc":{"id":"btc","city":"Burnaby","name":"Burnaby Tennis Club","website":"https://www.burnabytennis.ca/burnaby/home/login.do"}},"updated_at":"2022-11-30 20:45:43 -0800"};
+const runnerData = {
+  vacancies: {
+    btc: {
+      "Thu Dec 01, 2022": [
+        {
+          venue: "btc",
+          date: "Thu Dec 01, 2022",
+          start_time: "09:00 AM",
+          end_time: "10:00 AM",
+          duration: "1.0h",
+          court_info: "Indoor Court 3",
+        },
+        {
+          venue: "btc",
+          date: "Thu Dec 01, 2022",
+          start_time: "09:00 AM",
+          end_time: "10:00 AM",
+          duration: "1.0h",
+          court_info: "Indoor Court 5",
+        },
+        {
+          venue: "btc",
+          date: "Thu Dec 01, 2022",
+          start_time: "01:00 PM",
+          end_time: "02:00 PM",
+          duration: "1.0h",
+          court_info: "Indoor Court 2",
+        },
+      ],
+      "Fri Dec 02, 2022": [
+        {
+          venue: "btc",
+          date: "Fri Dec 02, 2022",
+          start_time: "08:00 AM",
+          end_time: "08:30 AM",
+          duration: "0.5h",
+          court_info: "Indoor Court 1",
+        },
+        {
+          venue: "btc",
+          date: "Fri Dec 02, 2022",
+          start_time: "09:00 AM",
+          end_time: "09:30 AM",
+          duration: "0.5h",
+          court_info: "Indoor Court 1",
+        },
+        {
+          venue: "btc",
+          date: "Fri Dec 02, 2022",
+          start_time: "05:00 PM",
+          end_time: "05:30 PM",
+          duration: "0.5h",
+          court_info: "Indoor Court 1",
+        },
+        {
+          venue: "btc",
+          date: "Fri Dec 02, 2022",
+          start_time: "10:00 PM",
+          end_time: "10:30 PM",
+          duration: "0.5h",
+          court_info: "Indoor Court 1",
+        },
+      ],
+    },
+  },
+  venues: {
+    btc: {
+      id: "btc",
+      city: "Burnaby",
+      name: "Burnaby Tennis Club",
+      website: "https://www.burnabytennis.ca/burnaby/home/login.do",
+    },
+  },
+  updated_at: "2022-11-30 22:07:03 -0800",
+};
