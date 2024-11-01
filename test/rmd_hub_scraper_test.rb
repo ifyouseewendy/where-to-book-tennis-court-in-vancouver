f6f721078c0ce91fa8731fb9248e84668803ba69
Di Wen <di@cointracker.com>
2024-11-01 10:08:44 -0700

    fixup! Disabled VAN and RMD courts checking

--- test/rmd_hub_scraper_test.rb
+++ test/rmd_hub_scraper_test.rb
@@ -5,7 +5,7 @@ class RmdHubScraperTest < Minitest::Test
     @scraper = RmdHubScraper.new
   end
 
-  def test_run
+  def skip_test_run
     VCR.use_cassette('rmd_hub_request_calendar_page') do
       Timecop.freeze(Date.parse('2022-12-06')) do
         vacancies = @scraper.run
